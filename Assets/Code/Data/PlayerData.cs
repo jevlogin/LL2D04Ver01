9b41eee2f7441e26e6a7d1b2e6e13937d75ee664
jevlogin <jev.login@mail.ru>
2020-12-09 18:18:24 +0300

    исправил орфографическую ошибку.

--- Assets/Code/Data/PlayerData.cs
+++ Assets/Code/Data/PlayerData.cs
@@ -8,9 +8,9 @@ namespace JevLogin
     {
         #region Fields
 
-        public PlayerStruct PlayerStruct;
-        public PlayerComponents PlayerComponents;
-        public PlayerSettingsData PlayerSettingsData;
+        [Header("Настройки свойств игрока."), Space(20)] public PlayerStruct PlayerStruct;
+        [Header("Содержит компоненты. Заполняется из кода. НЕ ТРОГАТЬ!"), Space(20)] public PlayerComponents PlayerComponents;
+        [Header("Содержит всевозможные настройки для игрока."), Space(20)] public PlayerSettingsData PlayerSettingsData;
 
         #endregion
 

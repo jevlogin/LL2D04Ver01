eb76a0af2f74000db68c945a752d5611762bf630
jevlogin <jev.login@mail.ru>
2020-12-12 00:51:54 +0300

    Система инпута

---
+++ Assets/Code/UserInput/PCInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class PCInput : IInput
+    {
+        public string Name => nameof(PCInput);
+    }
+}

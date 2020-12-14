e932d8a077559cf78875f55b8110fd3418ca28be
jevlogin <jev.login@mail.ru>
2020-12-14 17:26:30 +0300

    _

---
+++ Assets/Code/Controller/RotationPlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class RotationPlayerController : IRotation, ILateExecute
+    {
+        private Transform _playerTransform;
+        private Camera _camera;
+
+        public RotationPlayerController(Transform transform, Camera camera)
+        {
+            _playerTransform = transform;
+            _camera = camera;
+        }
+
+        public void LateExecute(float deltaTime)
+        {
+            var direction = _camera.WorldToScreenPoint(_playerTransform.position) - Input.mousePosition;
+            Rotation(direction);
+        }
+
+        public void Rotation(Vector3 direction)
+        {
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            _playerTransform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+}

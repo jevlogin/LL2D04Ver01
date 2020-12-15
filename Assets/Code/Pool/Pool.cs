a9e4d6469b454f16cf61fc50a85f20ac27ddf667
jevlogin <jev.login@mail.ru>
2020-12-15 21:39:10 +0300

    Добавил новый класс sealed class Pool

---
+++ Assets/Code/Pool/Pool.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class Pool
+    {
+        public string Tag { get; private set; }
+        public GameObject Prefab { get; private set; }
+        public int Size { get; private set; }
+    }
+}

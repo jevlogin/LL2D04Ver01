25080c3948a7120e1d8c5f2fcc026b8416312d55
jevlogin <jev.login@mail.ru>
2020-12-17 11:44:17 +0300

    RF CC sealed class EnemyShipInitialization : IInitialization

--- Assets/Code/Controller/EnemyShipInitialization.cs
+++ Assets/Code/Controller/EnemyShipInitialization.cs
@@ -5,10 +5,15 @@ namespace JevLogin
 {
     public sealed class EnemyShipInitialization : IInitialization
     {
+        #region Fields
+
         private EnemyShipPool _enemyShipPool;
         private Enemy _enemyShip;
 
+        #endregion
 
+
+        #region Properties
 
         public EnemyShipInitialization(EnemyShipPool enemyShipPool)
         {
@@ -17,11 +22,18 @@ namespace JevLogin
 
         public Enemy EnemyShip { get => _enemyShip; private set => _enemyShip = value; }
 
+        #endregion
+
+
+        #region IInitialization
+
         public void Initialization()
         {
             EnemyShip = _enemyShipPool.Get();
             EnemyShip.transform.position = new Vector2(Random.Range(-5, 5), Random.Range(-3, 3));
             EnemyShip.gameObject.SetActive(true);
         }
+
+        #endregion
     }
 }

f7259b8e91c81695fced23911a7dde245e4d38b6
jevlogin <jev.login@mail.ru>
2020-12-20 19:31:11 +0300

    написал public static class ServiceLocatorMonoBehaviour из методички но пока не понимаю что это!?!?

---
+++ Assets/Code/Interface/Test/ExampleServiceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class ExampleServiceLocator : MonoBehaviour
+    {
+        private void Awake()
+        {
+            ServiceLocator.SetService<IIservice>(new Service());
+        }
+
+        private void Start()
+        {
+            ServiceLocator.Resolve<IIservice>().Test();
+        }
+    }
+}
---
+++ Assets/Code/Interface/Test/Service.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    public sealed class Service : IIservice
+    {
+        public void Test()
+        {
+            Debug.Log(nameof(Service));
+        }
+    }
+}

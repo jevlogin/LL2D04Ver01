58b59902f31cf54a2c62c1f9fc32d53fd61f23f8
jevlogin <jev.login@mail.ru>
2020-12-20 19:08:19 +0300

    public static T DeepCopy<T>(this T self) created class extentions

--- Assets/Code/Helper/BuilderExtentions.cs
+++ Assets/Code/Helper/BuilderExtentions.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
 
 
 namespace JevLogin
@@ -7,6 +9,25 @@ namespace JevLogin
     {
         #region Methods
 
+        public static T DeepCopy<T>(this T self)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new System.ArgumentException("Type must be iserializeble");
+            }
+            if (ReferenceEquals(self, null))
+            {
+                return default;
+            }
+
+            var formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                formatter.Serialize(stream, self);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
         public static GameObject AddSprite(this GameObject gameObject, Sprite sprite)
         {
             var component = gameObject.GetOrAddComponent<SpriteRenderer>();

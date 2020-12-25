f344e72dbb8e3fd7bbda1e92219a6e61522b9475
jevlogin <jev.login@mail.ru>
2020-12-25 21:50:13 +0300

    Вывел словарь в инспектор через ISerializationCallbackReceiver и расширение UnityEditor

---
+++ Assets/Code/Data/DictionaryScriptableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JevLogin
+{
+    [CreateAssetMenu(fileName = "New Dictionary Data", menuName = "Data/DictionaryData", order = 51)]
+    public sealed class DictionaryScriptableObject : ScriptableObject
+    {
+        [SerializeField]
+        private List<int> _keys = new List<int>();
+
+        [SerializeField]
+        private List<string> _values = new List<string>();
+
+        public List<int> Keys { get => _keys; set => _keys = value; }
+        public List<string> Values { get => _values; set => _values = value; }
+    }
+}

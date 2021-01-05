099b65f2a66a34beb107a5a37b81117d5ac5b36a
jevlogin <jev.login@mail.ru>
2021-01-05 14:25:33 +0300

    Facade - самый легкий из всего пока что

---
+++ Assets/Code/Lesson/Facade/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace JevLogin.Facade
+{
+    public sealed class Game
+    {
+        public Game(Map map)
+        {
+            //...
+        }
+
+        public void Start()
+        {
+            //...
+        }
+    }
+}

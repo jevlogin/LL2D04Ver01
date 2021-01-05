b6adf814256710f787e70ed532c9732f00208520
jevlogin <jev.login@mail.ru>
2021-01-05 14:13:18 +0300

    Доделываю декортатор

--- Assets/Code/Lesson/Decorator/Example.cs
+++ Assets/Code/Lesson/Decorator/Example.cs
@@ -14,10 +14,26 @@ namespace JevLogin.Decorator
         [SerializeField] private AudioSource _audioSource;
         [SerializeField] private AudioClip _audioClip;
 
+        [Header("Muffler Gun")]
+        [SerializeField] private AudioClip _audioClipMuffler;
+        [SerializeField] private float _volumeFireOnMuffler;
+        [SerializeField] private Transform _barrelPositionMuffler;
+        [SerializeField] private GameObject _muffler;
+
         private void Start()
         {
             IAmmunition ammunition = new Bullet(_bullet, 3.0f);
-            _fire = new Weapon(_barrelPosition, _audioClip, _audioSource, ammunition, 999.0f);
+            var weapon = new Weapon(_barrelPosition, _audioClip, _audioSource, ammunition, 999.0f);
+
+            var muffler = new Muffler(_muffler, _volumeFireOnMuffler, _audioClipMuffler, _barrelPositionMuffler);
+
+            ModificationWeapon modificationWeapon = new ModificationMuffler(_audioSource, muffler, _barrelPositionMuffler.position);
+            modificationWeapon.ApplyModification(weapon);
+
+            _fire = modificationWeapon;
+
+            //_fire = weapon;
+
         }
 
         private void Update()
---
+++ Assets/Code/Lesson/Decorator/ModificationMuffler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+namespace JevLogin.Decorator
+{
+    public sealed class ModificationMuffler : ModificationWeapon
+    {
+        private readonly AudioSource _audioSource;
+        private readonly IMuffler _muffler;
+        private readonly Vector3 _mufflerPosition;
+
+        public ModificationMuffler(AudioSource audioSource, IMuffler muffler, Vector3 mufflerPosition)
+        {
+            _audioSource = audioSource;
+            _muffler = muffler;
+            _mufflerPosition = mufflerPosition;
+        }
+
+        protected override Weapon AddModification(Weapon weapon)
+        {
+            var muffler = Object.Instantiate(_muffler.MufflerInstance, _mufflerPosition, Quaternion.Euler(90.0f, 0.0f, 0.0f));
+            _audioSource.volume = _muffler.VolumeFireOnMuffler;
+            weapon.SetAudioClip(_muffler.AudioClipMuffler);
+            weapon.SetBarrelPosition(_muffler.BarrelPositionMuffler);
+            return weapon;
+        }
+
+    }
+}

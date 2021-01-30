9acc4a99ebf087b5b57ce399c0ea8a8dba55226a
jevlogin <jev.login@mail.ru>
2021-01-30 17:25:32 +0300

    Memento

---
+++ Assets/Code/Lesson/L06/Memento/PointInTime.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+namespace JevLogin.Memento
+{
+    public sealed class PointInTime
+    {
+        #region Fields
+
+        public Vector3 Position;
+        public Quaternion Rotation;
+        public Vector3 Velocity;
+        public Vector3 AngularVelocity;
+
+        #endregion
+
+
+        #region ClassLifeCycles
+
+        public PointInTime(Vector3 position, Quaternion rotation, Vector3 velocity, Vector3 angularVelocity)
+        {
+            Position = position;
+            Rotation = rotation;
+            Velocity = velocity;
+            AngularVelocity = angularVelocity;
+        }
+
+        #endregion
+    }
+}

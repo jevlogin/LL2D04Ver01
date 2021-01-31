30a43a5757b6e2fab3ec6bf6fac5f511380f8ff5
jevlogin <jev.login@mail.ru>
2021-01-31 19:43:40 +0300

    Pattern State

---
+++ Assets/Code/Lesson/L07/State/StateTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+namespace JevLogin.Lesson07.State
+{
+    public sealed class StateTest : MonoBehaviour
+    {
+        private void Start()
+        {
+            Context context = new Context(new ConcreteStateA());
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+        }
+    }
+}

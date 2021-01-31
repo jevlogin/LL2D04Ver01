4463d0bdfc3a5c1804980bc3429419c7a4c01cf3
jevlogin <jev.login@mail.ru>
2021-01-31 20:23:57 +0300

    Pattern Strategy

---
+++ Assets/Code/Lesson/L07/Strategy/BaseBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+namespace JevLogin.Lesson07.Strategy
+{
+    public abstract class BaseBehaviour : ScriptableObject
+    {
+        #region Fields
+
+        [SerializeField] protected float _speed;
+
+        #endregion
+
+
+        #region Methods
+
+        public abstract void Behaviour(Transform value);
+
+        #endregion
+    }
+}
